7ba56f7cd2a3a8fdeaee8d473f72c00c626ad264
riinakahila <[riina.kahila@myy.haaga-helia.fi]>
2025-08-27 15:11:57 +0300

    harjoitus1 valmis ja tarkistettu

--- src/main/java/harjoitus1/tehtava1/web/myController.java
+++ src/main/java/harjoitus1/tehtava1/web/myController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 public class myController {
 
-    @RequestMapping("/main")
+    @RequestMapping("/hello")
     @ResponseBody
 
     public String returnMessage( 
